b983cffafe6a3f8fe335c924531b89550f1f0485
Kireck211 <erickdesantiago11@gmail.com>
2016-09-21 21:04:28 -0500

    Modifying ui of sign in panel

--- src/main/java/iteso/mx/UI/SingInPanel.java
+++ src/main/java/iteso/mx/UI/SingInPanel.java
@@ -1,6 +1,7 @@
 package iteso.mx.UI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SingInPanel extends JPanel{
     private JLabel userJLabel;
@@ -12,18 +13,68 @@ public class SingInPanel extends JPanel{
 
     public SingInPanel () {
         userJLabel = new JLabel("Usuario:");
+        userJTextField = new JTextField(15);
         passwordJLabel = new JLabel("Contraseña");
-        userJTextField = new JTextField();
         passwordJTextField = new JTextField();
-        singIn = new JButton("Iniciar Sesión");
         forgotten = new JButton("¿Olvidaste tu contraseña?");
+
+        SpringLayout tableLayout = new SpringLayout();
+
+
+
+        JPanel tableLabelTextField = new JPanel(tableLayout);
+
+        tableLabelTextField.add(userJLabel);
+        tableLabelTextField.add(userJTextField);
+
+//        tableLayout.putConstraint(SpringLayout.WEST, userJLabel, SpringLayout.NORTH, tableLayout.getConstraint(tableLabelTextField));
+
         setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
 
-        add(userJLabel);
-        add(passwordJLabel);
-        add(userJTextField);
-        add(passwordJTextField);
-        add(singIn);
-        add(forgotten);
+        add(tableLabelTextField);
+
+
+        /*
+        setLayout(new GridBagLayout());
+        GridBagConstraints gb = new GridBagConstraints();
+
+        gb.gridx = 0;
+        gb.gridy = 0;
+        gb.weightx = 1;
+        gb.weighty = 1;
+        gb.fill = GridBagConstraints.NONE;
+
+
+        add(userJLabel, gb);
+
+
+        gb.gridx = 1;
+        gb.fill = GridBagConstraints.HORIZONTAL;
+        gb.insets = new Insets(0,0,0,20);
+        add(userJTextField, gb);
+
+
+        gb.fill = GridBagConstraints.NONE;
+        gb.gridy = 2;
+        add(forgotten, gb);
+
+
+        gb.gridx = 0;
+        gb.gridy = 3;
+        gb.insets = new Insets(0,0,0,0);
+        add(passwordJLabel, gb);
+
+
+        gb.gridx = 2;
+        gb.gridy = 3;
+        add(passwordJTextField, gb);
+
+        /*singIn = new JButton("Iniciar Sesión");
+        gb.weightx = 1;
+        gb.fill = GridBagConstraints.HORIZONTAL;
+        gb.gridx = 1;
+        gb.gridy = 4;
+        gb.insets = new Insets(20,0,0,0);
+        add(singIn, gb);*/
     }
 }
