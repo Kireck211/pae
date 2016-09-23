51fc59cc3754c0a79bdabc47c0fa1d555e267690
Kireck211 <erickdesantiago11@gmail.com>
2016-09-23 14:09:03 -0500

    Creation of connection

--- src/main/java/iteso/mx/App.java
+++ src/main/java/iteso/mx/App.java
@@ -10,9 +10,24 @@ public class App {
         theModel = new Model();
         theView = new View();
         theController = new Controller(theModel,theView);
+        connect();
+
     }
 
     public static void main(String args[]) {
         App mainApp = new App();
+
+    }
+
+    public void connect() {
+        theModel.ds.setIntegratedSecurity(true);
+        theModel.ds.setServerName("localhost");
+        theModel.ds.setPortNumber(1433);
+        theModel.ds.setDatabaseName("Lab1");
+        try {
+            theModel.con = theModel.ds.getConnection();
+        } catch (Exception e) {
+
+        }
     }
 }
--- src/main/java/iteso/mx/Model.java
+++ src/main/java/iteso/mx/Model.java
@@ -1,4 +1,45 @@
 package iteso.mx;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+
+/**
+ * Created by Erick on 23/09/2016.
+ */
 public class Model {
+
+    public Connection con = null;
+    public CallableStatement cstmt = null;
+    public ResultSet rs = null;
+    public SQLServerDataSource ds;
+
+    public Model() {
+        ds = new SQLServerDataSource();
+    }
+/*
+    try {
+        // Establish the connection.
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setIntegratedSecurity(true);
+        ds.setServerName("localhost");
+        ds.setPortNumber(1433);
+        ds.setDatabaseName("AdventureWorks");
+        con = ds.getConnection();
+    }
+
+    // Handle any errors that may have occurred.
+	    	catch (Exception e) {
+        e.printStackTrace();
+    }
+
+	   	finally {
+        if (rs != null) try { rs.close(); } catch(Exception e) {}
+        if (cstmt != null) try { cstmt.close(); } catch(Exception e) {}
+        if (con != null) try { con.close(); } catch(Exception e) {}
+    }*/
+
 }
