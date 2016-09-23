d947612abd033702cba1772de180312987ecceda
Kireck211 <erickdesantiago11@gmail.com>
2016-09-23 10:05:48 -0500

    Modify Sign In UI

--- src/main/java/iteso/mx/UI/SingInPanel.java
+++ src/main/java/iteso/mx/UI/SingInPanel.java
@@ -14,57 +14,44 @@ public class SingInPanel extends JPanel{
     public SingInPanel () {
         userJLabel = new JLabel("Usuario:");
         userJTextField = new JTextField(15);
-        passwordJLabel = new JLabel("Contraseña");
+        passwordJLabel = new JLabel("Contraseña:");
         passwordJTextField = new JTextField(15);
         forgotten = new JButton("¿Olvidaste tu contraseña?");
+        singIn = new JButton("Iniciar Sesión");
 
         setLayout(new GridBagLayout());
         GridBagConstraints gb = new GridBagConstraints();
 
         gb.gridx = 0;
-        gb.gridy = 0;
+        gb.gridy = 2;
         gb.weightx = 1;
         gb.weighty = 1;
+        gb.insets = new Insets(50,0,0,0);
         gb.fill = GridBagConstraints.NONE;
-
-
         add(userJLabel, gb);
 
-        gb.gridy = 1;
+        gb.gridy = 3;
+        gb.insets = new Insets(0,0,0,0);
         add(passwordJLabel, gb);
-
 
         gb.gridx = 1;
-        gb.gridy = 0;
+        gb.gridy = 2;
         gb.fill = GridBagConstraints.HORIZONTAL;
-        gb.insets = new Insets(0,0,0,20);
         add(userJTextField, gb);
 
-        gb.insets = new Insets(0,0,0,0);
-
-
-        gb.fill = GridBagConstraints.NONE;
-        gb.gridy = 2;
-        add(forgotten, gb);
-
-
-        gb.gridx = 0;
         gb.gridy = 3;
-        gb.insets = new Insets(0,0,0,0);
-        add(passwordJLabel, gb);
-
-
-        gb.gridx = 2;
-        gb.gridy = 3;
-        gb.fill = GridBagConstraints.HORIZONTAL;
         add(passwordJTextField, gb);
 
-        singIn = new JButton("Iniciar Sesión");
-        gb.weightx = 2;
-        gb.fill = GridBagConstraints.HORIZONTAL;
-        gb.gridx = 0;
+        gb.fill = GridBagConstraints.NONE;
+        gb.gridx = 1;
         gb.gridy = 4;
-        gb.insets = new Insets(20,0,0,0);
+        add(forgotten, gb);
+
+        gb.gridwidth = 2;
+        gb.fill = GridBagConstraints.HORIZONTAL;
+        gb.insets = new Insets(0,20,0,20);
+        gb.gridx = 0;
+        gb.gridy = 5;
         add(singIn, gb);
     }
 }
