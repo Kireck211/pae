72d5f672425451b0d4d9df99378b5b1f830bdd18
Miguel Hernández <par9615@gmail.com>
2016-10-21 11:07:46 -0500

    Table model fixed

--- src/main/java/iteso/mx/TableModels/ModifySellTableModel.java
+++ src/main/java/iteso/mx/TableModels/ModifySellTableModel.java
@@ -1,36 +1,31 @@
 package iteso.mx.TableModels;
 
-import iteso.mx.Model;
-
-import javax.swing.event.TableModelListener;
 import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableModel;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.Vector;
 
 /**
  * Created by Aldo on 20/10/2016.
  */
 public class ModifySellTableModel extends AbstractTableModel {
+
     String[] names = {"Folio",
-            "Fecha",
-            "Empleado",
-            "Cliente",
-            "Subtotal",
-            "IVA",
-            "Total"};
-    Vector<String> columnNames;
+                      "Fecha",
+                      "Empleado",
+                      "Cliente",
+                      "Subtotal",
+                      "IVA",
+                      "Total"};
+
+    Vector<String>  columnNames;
     Vector<Vector<Object>> data;
 
-   public ModifySellTableModel() {
-
-
-       columnNames = (new Vector<String>(Arrays.asList(names)));
-       data = new Vector<Vector<Object>>();
-
-
+    public ModifySellTableModel(){
+        columnNames = (new Vector<String>(Arrays.asList(names)));
+        data = new Vector<Vector<Object>>();
     }
+
+
 
 
     @Override
@@ -45,12 +40,21 @@ public class ModifySellTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return data.elementAt(rowIndex).elementAt(columnIndex);
+        return (data.elementAt(rowIndex)).elementAt(columnIndex);
     }
 
     public void addRow(Vector<Object> row){
         data.add(row);
         fireTableRowsInserted(data.size() -1, data.size() - 1);
+    }
+
+    public void setValueAt(Object value, int row, int col) {
+        (data.elementAt(row)).elementAt(col);
+        fireTableCellUpdated(row, col);
+    }
+
+    public String getColumnName(int column){
+        return columnNames.elementAt(column);
     }
 
 
--- src/main/java/iteso/mx/UI/SalesTabs/ModifySell.java
+++ src/main/java/iteso/mx/UI/SalesTabs/ModifySell.java
@@ -10,26 +10,31 @@ import java.util.Vector;
 
 
 public class ModifySell extends JPanel{
-
-
+    ModifySellTableModel model;
+    JTable table;
+    JScrollPane scrollPane;
    public ModifySell(){
-       super(new GridLayout(1,0));
-       ModifySellTableModel model = new ModifySellTableModel();
+       super(new GridLayout(2,0));
+
+       model = new ModifySellTableModel();
+
        Vector<Object> v = new Vector<Object>(Arrays.asList((Object)"1", (Object)"04-09-2015",(Object)"Aldo",(Object)"Juan",(Object)"000",
        (Object)"123", (Object)"789"));
 
-       model.addRow(v);
-
-       JTable table = new JTable(model);
+        model.addRow(v);
 
 
-       table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+       table = new JTable(model);
 
+       scrollPane = new JScrollPane(table);
 
-       setLayout(new BorderLayout());
-       add(table.getTableHeader(), BorderLayout.PAGE_START);
-       add(table, BorderLayout.CENTER);
-       add(table);
+       table.setFillsViewportHeight(true);
+
+       add(scrollPane);
+       JPanel panel2 = new JPanel();
+       panel2.setSize(new Dimension(getWidth(),200));
+       add(panel2);
+
    }
 
 
